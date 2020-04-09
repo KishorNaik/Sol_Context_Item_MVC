406ff5f086d0d0d013db3bd0cf0793f8cc6c3a8a
KishorNaik <kishor.naik011.net@gmail.com>
2020-04-09 13:48:07 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sol_Demo.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Items.ContainsKey("MyValue")==true)
+            {
+                ViewBag.Data = HttpContext.Items["MyValue"] as string;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Startup.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Sol_Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.Use(async (context, next) =>
+            {
+                // Send Data from Middlware to Controller then always use context.Items
+                context.Items["MyValue"] = "Hello Asp.net Core";
+
+                await next.Invoke();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Demo}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
